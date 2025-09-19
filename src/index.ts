15c25e6809e90faf6643a65d3be542b2f61d2650
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2025-09-19 19:01:23 +0200

    ok; basic JSON validation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import Ajv from "ajv";
+
+const schema = await Bun.file('./schema.json').json()
+const target = await Bun.file('./data/mainstream.json').json()
+
+const ajv = new Ajv()
+const validate =ajv.compile(schema)
+const valid = validate(target)
+if (!valid){
+    console.log(validate.errors)
+}
