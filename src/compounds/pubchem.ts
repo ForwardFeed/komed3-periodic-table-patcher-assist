4277718ee6576d6e65cfbbdfcf8257e3812521d9
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2025-09-29 13:29:01 +0200

    started the process to fetch from pubchem

---
+++ src/compounds/pubchem.ts
@@ -0,0 +1,55 @@
+import {JSDOM} from "jsdom"
+import { does_cache_file_exist, fetch_and_write_to_cache, fetch_json_and_write_to_cache, get_from_cache, get_nist_compounds_data, write_to_cache } from "../filesystem_integration";
+import type { NIST_Compound } from "./nist";
+
+/**
+ * 
+ * https://www.ncbi.nlm.nih.gov/pccompound/?term=7631-86-9 <= in HTML search gives link to 
+ * https://pubchem.ncbi.nlm.nih.gov/compound/24261
+ * https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/24261/JSON/
+ * 
+ * => Chemical and Physical Properties
+ */
+
+async function download_with_(entry: NIST_Compound, filename: string, term: string): Promise<boolean>{
+    const URL = `https://www.ncbi.nlm.nih.gov/pccompound/?term=${term}`
+    try {
+        await fetch_and_write_to_cache(URL, filename)
+        const file_html = await get_from_cache(filename)
+        const dom = new JSDOM(file_html)
+        const document = dom.window.document
+        if (document.title ==  "No items found - PubChem Compound - NCBI")
+            return false
+        return true
+    }catch(e){
+        return false
+    }
+    
+}
+
+async function download_to_cache(){
+    const nist_entries = await get_nist_compounds_data()
+    for (const entry of nist_entries){
+        const filename = `compounds_${entry.original_formula}_pubchem_search.html`
+        // make sure we don't redownload it
+        if (await does_cache_file_exist(filename)){
+            continue
+        }
+        // if there's a cas prioritize it in search
+        if (entry.cas_number){
+            if (await download_with_(entry, filename, entry.cas_number))
+                continue
+        }
+        // if not, or if the cas didn't gave any result, try by name.
+        if (! await download_with_(entry, filename, entry.name)){
+            console.error(`failed to get information on: ${entry.original_formula}`)
+        }
+        // I'm scared of timeout
+        await Bun.sleep(0.4)
+    }
+    
+    // step two parse the searches
+    // step download the data in JSON from the REST API
+    // at https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/24261/JSON/
+}
+await download_to_cache()
